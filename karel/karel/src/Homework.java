4ee25ed7afd790f596a04c0181266371ea83037f
hamzaD99 <46914044+hamzaD99@users.noreply.github.com>
2021-05-01 18:36:43 +0300

    Editing some bugs and corner cases

--- karel/karel/src/Homework.java
+++ karel/karel/src/Homework.java
@@ -23,6 +23,7 @@ public class Homework extends SuperKarel {
         pickBeepers();
         backHome();
         level1();
+        backHome();
         pickBeepers();
         backHome();
         level2();
@@ -36,16 +37,12 @@ public class Homework extends SuperKarel {
 
     private boolean moveNorth() {
         boolean done = false;
-        int c = 0;
         if (facingEast()) {
             turnLeft();
-            c = 1;
         } else if (facingWest()) {
             turnRight();
-            c = 2;
         } else if (facingSouth()) {
             turnAround();
-            c = 3;
         }
         if (frontIsClear()) {
             customMove();
@@ -56,16 +53,12 @@ public class Homework extends SuperKarel {
 
     private boolean moveSouth() {
         boolean done = false;
-        int c = 0;
         if (facingEast()) {
             turnRight();
-            c = 1;
         } else if (facingWest()) {
             turnLeft();
-            c = 2;
         } else if (facingNorth()) {
             turnAround();
-            c = 3;
         }
         if (frontIsClear()) {
             customMove();
@@ -76,16 +69,12 @@ public class Homework extends SuperKarel {
 
     private boolean moveEast() {
         boolean done = false;
-        int c = 0;
         if (facingNorth()) {
             turnRight();
-            c = 1;
         } else if (facingSouth()) {
             turnLeft();
-            c = 2;
         } else if (facingWest()) {
             turnAround();
-            c = 3;
         }
         if (frontIsClear()) {
             customMove();
@@ -96,16 +85,12 @@ public class Homework extends SuperKarel {
 
     private boolean moveWest() {
         boolean done = false;
-        int c = 0;
         if (facingNorth()) {
             turnLeft();
-            c = 1;
         } else if (facingSouth()) {
             turnRight();
-            c = 2;
         } else if (facingEast()) {
             turnAround();
-            c = 3;
         }
         if (frontIsClear()) {
             customMove();
@@ -182,36 +167,23 @@ public class Homework extends SuperKarel {
 
     private int level1() {
         int c = 0;
-        for (int i = 0; i < map_x - 1; i++) {
-            moveEast();
-            if ((x + y) % 2 != 0) {
-                putBeeper();
-                c++;
+        int inner = 0;
+        if(map_y == 1 || map_x == 1) inner = 0;
+        else inner = (map_x-2)*(map_y-2);
+        int frame_len = (map_x*map_y)-inner;
+        int frameCount = frame_len;
+        while (frameCount>0){
+            if(frontIsClear()){
+                customMove();
+                frameCount--;
+                if ((x + y) % 2 != 0) {
+                    putBeeper();
+                    c++;
+                }
             }
-        }
-        for (int i = 0; i < map_y - 1; i++) {
-            moveNorth();
-            if ((x + y) % 2 != 0) {
-                putBeeper();
-                c++;
+            else {
+                turnLeft();
             }
-
-        }
-        for (int i = 0; i < map_x - 1; i++) {
-            moveWest();
-            if ((x + y) % 2 != 0) {
-                putBeeper();
-                c++;
-            }
-
-        }
-        for (int i = 0; i < map_y - 1; i++) {
-            moveSouth();
-            if ((x + y) % 2 != 0) {
-                putBeeper();
-                c++;
-            }
-
         }
         System.out.print("# of beepers in level 1: ");
         System.out.println(c);
@@ -222,7 +194,7 @@ public class Homework extends SuperKarel {
         int c = 0;
         for (int i = 1; i <= map_y; i++) {
             for (int j = 1; j <= map_x; j++) {
-                if ((x + y) % 2 == 0){
+                if ((x + y) % 2 == 0) {
                     putBeeper();
                     c++;
                 }
@@ -238,34 +210,66 @@ public class Homework extends SuperKarel {
         return c;
     }
 
-    private int level3(){
+    private int level3() {
         int c = 0;
         int mid_x = 0, mid_y = 0;
-        if(map_x%2 != 0){
-            mid_x = (map_x/2) + 1;
+        if(map_x == 1){
+            if (map_y % 2 != 0){
+                goTo(1,(map_y/2)+1);
+                putBeeper();
+                c++;
+            }
+            else {
+                goTo(1,map_y/2);
+                putBeeper();
+                goTo(1,(map_y/2)+1);
+                putBeeper();
+                c+=2;
+            }
         }
-        else if(map_x%2 == 0){
-            mid_x = (map_x/2);
+        else if(map_y == 1){
+            if (map_x % 2 != 0){
+                goTo((map_x/2)+1,1);
+                putBeeper();
+                c++;
+            }
+            else {
+                goTo(map_x/2,1);
+                putBeeper();
+                goTo((map_x/2)+1,1);
+                putBeeper();
+                c+=2;
+            }
         }
-        if(map_y%2 != 0){
-            mid_y = (map_y/2) + 1;
+        else {
+            if (map_x % 2 != 0) {
+                mid_x = (map_x / 2) + 1;
+            } else if (map_x % 2 == 0) {
+                mid_x = (map_x / 2);
+            }
+            c += putBeepersInLineX(mid_x);
+            if (map_x % 2 == 0) c += putBeepersInLineX(mid_x + 1);
+
+            if (map_y % 2 != 0) {
+                mid_y = (map_y / 2) + 1;
+            } else if (map_y % 2 == 0) {
+                mid_y = (map_y / 2);
+            }
+            c += putBeepersInLineY(mid_y);
+            if (map_y % 2 == 0) c += putBeepersInLineY(mid_y + 1);
         }
-        else if(map_y%2 == 0){
-            mid_y = (map_y/2);
-        }
-        c += putBeepersInLineX(mid_x);
-        if(map_x%2==0) c += putBeepersInLineX(mid_x+1);
-        c += putBeepersInLineY(mid_y);
-        if(map_y%2==0) c += putBeepersInLineY(mid_y+1);
+
+
         System.out.print("# of beepers in level 3: ");
         System.out.println(c);
         return c;
     }
-    private int putBeepersInLineX(int mid_x){
+
+    private int putBeepersInLineX(int mid_x) {
         int c = 0;
-        goTo(mid_x,1);
-        for (int i = 0; i<y; i++){
-            if(noBeepersPresent()) {
+        goTo(mid_x, 1);
+        for (int i = 0; i < y; i++) {
+            if (noBeepersPresent()) {
                 putBeeper();
                 c++;
             }
@@ -273,11 +277,12 @@ public class Homework extends SuperKarel {
         }
         return c;
     }
-    private int putBeepersInLineY(int mid_y){
+
+    private int putBeepersInLineY(int mid_y) {
         int c = 0;
-        goTo(1,mid_y);
-        for (int i = 0; i<x; i++){
-            if(noBeepersPresent()) {
+        goTo(1, mid_y);
+        for (int i = 0; i < x; i++) {
+            if (noBeepersPresent()) {
                 putBeeper();
                 c++;
             }
@@ -285,6 +290,7 @@ public class Homework extends SuperKarel {
         }
         return c;
     }
+
 }
 
 
